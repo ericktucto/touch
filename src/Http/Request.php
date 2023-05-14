33b399a78f8a67479445960666f9ce74a51e1401
Erick Tucto <erick@ericktucto.com>
2023-05-14 12:39:15 -0500

    Agregando validacion con vlucas/valitron

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,19 +4,27 @@ namespace Touch\Http;
 
 use GuzzleHttp\Psr7\ServerRequest;
 use Psr\Http\Message\ServerRequestInterface;
+use stdClass;
 
 class Request extends ServerRequest implements ServerRequestInterface
 {
-  public function query(string $input, $default = null)
+  public function query(?string $input = null, $default = null)
   {
     $query = $this->getQueryParams();
+
+    if (is_null($input)) return $query;
+
     return $query[$input] ?? $default;
   }
 
-  public function body(string $input, $default = null)
+  public function body(?string $input = null, $default = null)
   {
     $body = $this->getBody();
     $data = json_decode($body->read($body->getSize()));
+
+    if (is_null($input)) {
+      return is_null($data) ? new stdClass : $data;
+    }
 
     if (is_null($data) || !property_exists($data, $input)) {
       return $default;
---
+++ src/Http/ValidRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Touch\Http;
+
+use Valitron\Validator;
+
+trait ValidRequest
+{
+  protected Validator $valitron;
+  protected function validate(array|object $data, array $rules): bool
+  {
+    $data = is_array($data) ? $data : get_object_vars($data);
+    $this->valitron = new Validator($data);
+    foreach ($rules as $field => $rule) {
+      $this->valitron->mapFieldRules($field, $rule);
+    }
+    return $this->valitron->validate();
+  }
+}
